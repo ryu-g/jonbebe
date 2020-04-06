ca09c78143a203461ae8a51d9db03a232ac9a8c1
r.murayama <r.murayama@admins-MacBook-Pro.local>
2020-04-06 13:26:47 +0900

    add debris

--- script/index.js
+++ script/index.js
@@ -22,3 +22,17 @@ function addSpeed(){
   rotateDurationOfEarth *= 0.8
   earth.style.animation = `rotateEarth ${rotateDurationOfEarth}s linear infinite`
 }
+
+window.addEventListener( "scroll", updateDebris)
+
+function updateDebris(){
+	let y = window.pageYOffset
+	console.log(y)
+	const debris = document.getElementsByClassName("debris")
+	for(let i = 0 ; i < debris.length; i ++){
+		let top = 800 - (y/4) * 2
+		debris[i].style.top = top+"px"
+		debris[i].style.left = y+"px"
+	}
+	console.log(debris)	
+}
