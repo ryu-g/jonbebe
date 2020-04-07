18e00af02ba79dde3cd9f39782db1ec448ba084f
ryu-g <em.ryu.g@gmail.com>
2020-04-07 15:20:06 +0900

    add method for debris

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 import "../style/main.sass"
 
 let rotateDurationOfEarth = 7
+const height = window.innerHeight
+const width = window.innerWidth
 
 window.onload = () => {
   const stars = document.getElementsByClassName("star")
@@ -26,13 +28,33 @@ function addSpeed(){
 window.addEventListener( "scroll", updateDebris)
 
 function updateDebris(){
-	let y = window.pageYOffset
-	console.log(y)
-	const debris = document.getElementsByClassName("debris")
-	for(let i = 0 ; i < debris.length; i ++){
-		let top = 800 - (y/4) * 2
-		debris[i].style.top = top+"px"
-		debris[i].style.left = y+"px"
-	}
-	console.log(debris)	
+  const scrollY = window.pageYOffset / 1000 + 2000
+  debri_swing(scrollY, "debris")
+  debri_Oval(scrollY, "alien", width / 2, height / 2, 1200, 300 )
+  debri_Oval(scrollY, "sun", 0, 0, 800, 900, 0.1 )
+}
+
+function debri_Oval(scrollY, className, x, y, w, h, _speed){
+  const debris = document.getElementsByClassName(className)
+  let speed = 1
+  if(_speed){speed = _speed}
+  let cos = Math.cos(scrollY * speed)
+  let sin = Math.sin(scrollY * speed)
+  let top = sin * h + y
+  let left = cos * w + x
+  for(let i = 0 ; i < debris.length; i ++){
+    debris[i].style.top = top + "px"
+    debris[i].style.left = left + "px"
+  }
+}
+
+
+function debri_swing(scrollY, className){
+ 
+  const debris = document.getElementsByClassName(className)
+  for(let i = 0 ; i < debris.length; i ++){
+    let top = window.innerHeight - (scrollY/18) ** 1.5
+    debris[i].style.top = top+"px"
+    debris[i].style.left = scrollY+"px"
+  }
 }
